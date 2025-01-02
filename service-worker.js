a9f266f0fbde7b7f890eec35a357d38c40fc59ec
YagamiLight055 <130136055+YagamiLight055@users.noreply.github.com>
2025-01-02 15:52:09 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('batt-soc-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'styles.css',
+        'script.js',
+        'battsoc.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
