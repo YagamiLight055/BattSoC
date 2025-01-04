eee3dc8204b4d4b943bd142e7135d3e68ab683fd
YagamiLight055 <130136055+YagamiLight055@users.noreply.github.com>
2025-01-04 12:47:02 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // Register Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('scripts/service-worker.js').then((registration) => {
+    navigator.serviceWorker.register('/scripts/service-worker.js').then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch((error) => {
       console.log('Service Worker registration failed:', error);
